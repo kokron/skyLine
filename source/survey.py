4d64cd80582dcff1a7c3db8dc062812ff8ad96d9
jl-bernal <bernal.mera.jl@gmail.com>
2022-10-25 10:47:18 +0200

    fixed bug in the filter for sigma_par

--- source/survey.py
+++ source/survey.py
@@ -1260,7 +1260,7 @@ def aniso_filter(k, v):
     kk2_perp = newk[1]**2 + newk[2]**2
     
     if rpar > 0:
-        w = np.exp(-0.5*kk2_perp * rper**2)*rpar*np.sinc(newk[0]*rpar/2)
+        w = np.exp(-0.5*kk2_perp * rper**2)*np.sinc(newk[0]*rpar/2)
     else:
         w = np.exp(-0.5*kk2_perp * rper**2)
 
